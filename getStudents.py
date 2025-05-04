4bdf02e308d58ac6ebfe71206e4969a4d40e6b6b
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-05-04 15:08:43 +0630

    add signoz

--- getStudents.py
+++ getStudents.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
     response = table.scan()
     data = response['Items']
 
-    # If there are more items to scan, continue scanning until all items are retrieved!
+    # If there are more items to scan, continue scanning until all items are retrieved
     while 'LastEvaluatedKey' in response:
         response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
         data.extend(response['Items'])
